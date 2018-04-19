23392235079dd5d5791acded48eb6aa4bbc86336
Yaspesberro <yaspesberro@gmail.com>
2018-04-19 12:00:04 -0700

    Added query contents in app.get and reinstalled node_modules

--- rhiannon-yannick/server.js
+++ rhiannon-yannick/server.js
@@ -10,14 +10,15 @@ const app = express();
 // Windows and Linux users: You should have retained the user/password from the pre-work for this course.
 // Your OS may require that your conString is composed of additional information including user and password.
 // const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';
+const conString = 'postgres://postgres:password@localhost:8080/kilovolt';
 
 // Mac:
-const conString = 'postgres://localhost:5432';
+//const conString = 'postgres://localhost:5432';
 
-const client = new pg.Client();
+const client = new pg.Client(conString);
 
 // REVIEW: Use the client object to connect to our DB.
-client.connect(conString);
+client.connect();
 
 
 // REVIEW: Install the middleware plugins so that our app can parse the request body
@@ -38,7 +39,7 @@ app.get('/new', (request, response) => {
 app.get('/articles', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // n.3 of the pic. there is a query method being applied to create rows on the result, otherwise it would throw an error. in crud this one would be update because it updates the data query (result.rows add a row to the query)
-  client.query('')
+  client.query('SELECT * FROM articles')
     .then(function (result) {
       response.send(result.rows);
     })
